045dc1ac4d70743078916e334b8a6b7d6ae58267
Harry Merritt <harry@merritt.es>
2020-03-06 01:06:08 +0000

    Change button text when item is added to the cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { indexOf } from "lodash";
 
 const Product = props => {
-	return (
-		<div className="product">
-			<img src={props.product.image} alt={`${props.product.title} book`} />
 
-			<h1 className="title">{props.product.title}</h1>
+	const { cart } = useContext(CartContext);
 
-			<p className="price">${props.product.price}</p>
+    return (
+        <div className="product">
+            <img
+                src={props.product.image}
+                alt={`${props.product.title} book`}
+            />
 
-			<button onClick={() => props.addItem(props.product)}>
-				Add to cart
-			</button>
-		</div>
-	);
+            <h1 className="title">{props.product.title}</h1>
+
+            <p className="price">${props.product.price}</p>
+
+            <button onClick={() => props.addItem(props.product)}>
+                {
+					(() => {
+						if (indexOf(cart, props.product) === -1) {
+							return "Add to cart";
+						}
+						else {
+							return "Added to cart";
+						}
+					})()
+				}
+            </button>
+        </div>
+    );
 };
 
 export default Product;
